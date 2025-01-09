d6dd68f2780fcc27f53fde0e73aa02f439a8fa47
angeloalmanza <angeloalmanza26@gmail.com>
2025-01-09 09:43:18 +0100

    refactor immagini

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,9 +1,11 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const AppCard = ({post, onCancel}) => {
+    const imagePath = post.image.replace("img/", "");
+    
     return (
         <div className="card mb-3">
-            <img src={`${apiUrl}/${post.image}`} className="card-img-top h-50"/>
+            <img src={`${apiUrl}/${imagePath}`} className="card-img-top h-50"/>
             <div className="card-body">
               <h4>{post.title}</h4>
               <p>{post.content}</p>
